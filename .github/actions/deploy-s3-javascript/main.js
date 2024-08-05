a2ca64a951721c0276be03a06b655b72e8822b5c
MiKayla Chinn <mikayla.chinn@gm.com>
2024-08-05 11:20:53 -0500

    Removed extra text in main.js

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -11,7 +11,6 @@ function run() {
 
     // 2) Upload files
     const s3Uri = `s3://${bucket}`
-    AWS_ACCESS_KEY_ID
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
 
     core.notice('Hello from my custom JavaScript Action!');
